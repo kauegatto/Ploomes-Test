3cdfc2306d4329405869789fb59c05dbe6b51601
kaue gatto <kauegatto123@gmail.com>
2024-02-20 15:50:07 -0300

    db: migration to make assignee email not required

---
+++ Ploomes-Test.Infrastructure/Migrations/20240220184804_FixAssigneeNotRequired.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ploomes_Test.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixAssigneeNotRequired : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
