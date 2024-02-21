61174c1967d2154d30d7d4a4dc982fb9ea254508
kaue gatto <kauegatto123@gmail.com>
2024-02-21 18:59:18 -0300

    db : add migrations

---
+++ Ploomes-Test.Infrastructure/Migrations/20240221214113_DateTimeFields.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ploomes_Test.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class DateTimeFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreateDate",
+                table: "Ticket",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "EndedAt",
+                table: "Ticket",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "StartedAt",
+                table: "Ticket",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "UpdateDate",
+                table: "Ticket",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreateDate",
+                table: "Ticket");
+
+            migrationBuilder.DropColumn(
+                name: "EndedAt",
+                table: "Ticket");
+
+            migrationBuilder.DropColumn(
+                name: "StartedAt",
+                table: "Ticket");
+
+            migrationBuilder.DropColumn(
+                name: "UpdateDate",
+                table: "Ticket");
+        }
+    }
+}
